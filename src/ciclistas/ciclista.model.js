b5addcd8cc1554142c47fe3c4d2e3fd26a66d0cb
Sergio Marcilla <sergio@devgurus.io>
2022-07-15 16:44:58 +0200

    adds ciclistas entity

---
+++ src/ciclistas/ciclista.model.js
@@ -0,0 +1,31 @@
+class Ciclista {
+    constructor(etapa, dado){
+        this.kilometros = 0;
+        this.tiempoEtapa = 0;
+        this.tiempoCarrera = 0;
+        this.etapa = etapa;
+        this.dado = dado;
+    }
+    avanzarKilometros(avance){
+        this.kilometros = this.kilometros + avance;
+    }
+    sumarTiempoEnEtapa(){
+        this.tiempoEtapa = this.etapa.calcularTiempo(this.kilometros);
+    }
+    sumarTiempoEnCarrera(){
+        this.tiempoCarrera = this.tiempoCarrera + this.tiempoEtapa;
+    }
+    avanzarEnEtapa(){
+        const avance = this.dado.lanzar();
+        this.avanzarKilometros(avance);
+        this.sumarTiempoEnEtapa();
+        this.sumarTiempoEnCarrera();
+    }
+    obtenerTiempoEnEtapa(){
+        return this.tiempoEtapa;
+    }
+    obtenerTiempoEnCarrera(){
+        return this.tiempoCarrera;
+    }
+}
+module.exports = Ciclista;
