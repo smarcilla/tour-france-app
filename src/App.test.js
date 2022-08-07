103bd45c98e66c0802488cd433415ef029cc7459
xus84 <g_garsan@hotmail.com>
2022-08-08 00:28:21 +0200

    frontend changes & others

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the title of App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1 = screen.getByText(/Tour of France 2022./i);
+  expect(h1).toBeInTheDocument();
 });
